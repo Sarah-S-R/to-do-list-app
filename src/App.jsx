39101452686fc76a94b40d1ba5e6e9b6c79b73c3
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2024-04-06 09:21:08 -0230

    added list items and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import "./styles.css"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      
-    </>
-  )
+export default function App () {
+  return <>
+  
+  <form className = "new-item-form">
+  </form>
+  <h1 className = "header">Todo List</h1>
+  <ul className = "list">
+    <li>
+      <label>
+        <input type = "checkbox"></input>
+        Item 1
+      </label>
+      <button className = "Btn Btn-danger">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type = "checkbox"></input>
+        Item 1
+      </label>
+      <button className = "Btn Btn-danger">Delete</button>
+    </li>
+
+  </ul>
+ </>
+
 }
-
-export default App
